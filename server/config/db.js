a249a38b311f3db56067aca1caae9156e05e6195
singhmoni <mkumari5@my.centennialcollege.ca>
2022-03-07 02:04:51 -0500

    web App

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://scooby:scoobydooby@cluster0.2mpdu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+};
---
+++ server/routes/books.js
@@ -0,0 +1,125 @@
+// modules required for routing
+let express = require('express');
+let router = express.Router();
+let mongoose = require('mongoose');
+
+// define the book model
+let book = require('../models/books');
+
+/* GET books List page. READ */
+router.get('/', (req, res, next) => {
+  // find all books in the books collection
+  book.find( (err, books) => {
+    if (err) {
+      return console.error(err);
+    }
+    else {
+      res.render('books/index', {
+        title: 'Books',
+        books: books
+      });
+    }
+  });
+
+});
+
+//  GET the Book Details page in order to add a new Book
+router.get('/add', (req, res, next) => {
+  res.render('books/details', {
+    title: 'Add a Book',
+    books: '',
+    action: '/books/add'
+  });
+    /*****************
+     * ADD CODE HERE *
+     *****************/
+});
+
+// POST process the Book Details page and create a new Book - CREATE
+router.post('/add', (req, res, next) => {
+  let data = req.body;
+  // Formats data accordinly 
+  const newBook = {
+    Title: data.title,
+    Description: data.description,
+    Price: parseInt(data.price),
+    Author: data.author,
+    Genre: data.genre
+  }
+  // Creates the book on MongoDB
+  book.create(newBook, function(err, result) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.redirect('/books');
+    }
+  });
+    /*****************
+     * ADD CODE HERE *
+     *****************/
+
+});
+
+// GET the Book Details page in order to edit an existing Book
+router.get('/:id', (req, res, next) => {
+  book.findById( req.params.id , (err, book) => {
+    if (err) {
+      return console.error(err);
+    }
+    else {
+      // Redirects user to Details Page
+      res.render('books/details', {
+        title: 'Edit a Book',
+        books: book,
+        action: ''
+      });
+    }
+  });
+    /*****************
+     * ADD CODE HERE *
+     *****************/
+});
+
+// POST - process the information passed from the details form and update the document
+router.post('/:id', (req, res, next) => {
+  let data = req.body;
+  // Formats data accordinly 
+  const upsertData = {
+    Title: data.title,
+    Description: data.description,
+    Price: parseInt(data.price),
+    Author: data.author,
+    Genre: data.genre
+  }
+  book.update( {_id: req.params.id} , upsertData, {upsert: true}, (err, result) => {
+    if (err) {
+      return console.error(err);
+    }
+    else {
+      res.redirect('/books');
+    }
+  });
+    /*****************
+     * ADD CODE HERE *
+     *****************/
+
+});
+
+// GET - process the delete by user id
+router.get('/delete/:id', (req, res, next) => {
+  book.remove( {_id: req.params.id} , (err) => {
+    if (err) {
+      return console.error(err);
+    }
+    else {
+      res.redirect('/books');
+    }
+  });
+
+    /*****************
+     * ADD CODE HERE *
+     *****************/
+});
+
+
+module.exports = router;
